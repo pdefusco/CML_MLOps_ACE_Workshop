abdabcb0ba516d7c94e95cf36a86a6545926cbcf
pdefusco <pauldefusco@gmail.com>
2023-01-18 10:56:33 -0800

    backing up

--- 03_MLOps_Implementation/script.py
+++ 03_MLOps_Implementation/script.py
@@ -4,11 +4,14 @@ import pandas as pd
 import numpy as np
 from cmlops import cmlops_lib
 
-base_model_file_path = "/home/cdsw/models/"
+base_model_file_path = "/home/cdsw/03_MLOps_Implementation/models/"
 base_model_script_path = base_model_file_path + "development_model.sav"
-base_model_training_data_path = "/home/cdsw/data/X_train.csv"
+base_model_training_data_path = "/home/cdsw/03_MLOps_Implementation/data/X_train.csv"
 project_id = os.environ["CDSW_PROJECT_ID"]
-model_name =
+#model_name = "marketing"
 function_name = "predict"
 
-cmlPipeline = ProductionModelPipeline(base_model_file_path, base_model_script_path, base_model_training_data_path, project_id, model_name, function_name)
+prodPipeline = CMLProductionPipeline(base_model_file_path, base_model_script_path, base_model_training_data_path, project_id, function_name)
+
+
+prodPipeline
