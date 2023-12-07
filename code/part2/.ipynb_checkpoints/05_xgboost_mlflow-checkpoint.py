710c7231a095ef42b4102f2e7c7f0b57abdd1fa9
pdefusco <pauldefusco@gmail.com>
2023-12-06 20:03:43 -0800

    proj updates

--- code/part2/.ipynb_checkpoints/05_xgboost_mlflow-checkpoint.py
+++ code/part2/.ipynb_checkpoints/05_xgboost_mlflow-checkpoint.py
@@ -40,73 +40,52 @@
 import os
 import warnings
 import sys
+import xgboost
 import mlflow
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import ElasticNet
+from sklearn.metrics import accuracy_score, recall_score
 import mlflow.sklearn
 import logging
+from xgboost import XGBClassifier
 
-username = "pauldefusco"
+mlflow.set_experiment(experiment_name="heart-clf")
 
-logging.basicConfig(level=logging.WARN)
-logger = logging.getLogger(__name__)
+df = pd.read_csv("data/heart.csv")
+       
+df["thal"] = df["thal"].astype("category").cat.codes                 
+df["thal"].unique()
+  
+X_train, X_test, y_train, y_test = train_test_split(
+    df.drop("target", axis=1), df["target"], test_size=0.3
+)
+                 
+with mlflow.start_run():
 
-def eval_metrics(actual, pred):
-    rmse = np.sqrt(mean_squared_error(actual, pred))
-    mae = mean_absolute_error(actual, pred)
-    r2 = r2_score(actual, pred)
-    return rmse, mae, r2
+    model = XGBClassifier(use_label_encoder=False, eval_metric="logloss")
+  
+    model.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=False)
 
-if __name__ == "__main__":
-    warnings.filterwarnings("ignore")
-    np.random.seed(40)
+    y_pred = model.predict(X_test)
 
-    mlflow.set_experiment("wine-quality-test")
+    accuracy = accuracy_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)                 
 
-    # Read the wine-quality csv file from the URL
-    csv_url = (
-        "https://raw.githubusercontent.com/mlflow/mlflow-example/master/wine-quality.csv"
-    )
-    try:
-        data = pd.read_csv(csv_url, sep=",")
-    except Exception as e:
-        logger.exception(
-            "Unable to download training & test CSV, check your internet connection. Error: %s", e
-        )
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    print("Recall: %.2f%%" % (recall * 100.0))
+  
+    mlflow.log_param("accuracy", accuracy)
+    mlflow.log_param("recall", recall)
+    mlflow.xgboost.log_model(model, artifact_path="artifacts")#, registered_model_name="my_xgboost_model"
+        
+#Replace Experiment Run ID here:
+run = mlflow.get_run("9m9m-mrhb-t7h1-i0v2")
 
-    # Split the data into training and test sets. (0.75, 0.25) split.
-    train, test = train_test_split(data)
-
-    # The predicted column is "quality" which is a scalar from [3, 9]
-    train_x = train.drop(["quality"], axis=1)
-    test_x = test.drop(["quality"], axis=1)
-    train_y = train[["quality"]]
-    test_y = test[["quality"]]
-
-    alpha = 0.4
-    l1_ratio = 0.2
-
-    with mlflow.start_run():
-
-        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-        lr.fit(train_x, train_y)
-
-        predicted_qualities = lr.predict(test_x)
-
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
-
-        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
-
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
-        #mlflow.sklearn.log_model(lr, artifact_path="artifacts")
-        mlflow.sklearn.log_model(lr, artifact_path="artifacts", registered_model_name="wine-clf-"+username)
+pd.DataFrame(data=[run.data.params], index=["Value"]).T
+                 
+pd.DataFrame(data=[run.data.metrics], index=["Value"]).T
+                 
+client = mlflow.tracking.MlflowClient()
+client.list_artifacts(run_id=run.info.run_id)
+                                
